9e556ee1c15f5979fe0b7884a1475d020dede971
Nick Gard <ngard@walmartlabs.com>
2016-11-10 14:33:16 -0800

    Simplify to only take 'shadow-element'

--- index.js
+++ index.js
@@ -1,121 +1,61 @@
 var postcss = require('postcss');
 
-/*
-  TODO:
-  0. prevent name collisions. Disambiguate selectors.
-  1. shadow-root
-    a. only allowed on single class/attribute selector
-    b. disallow page styles to flow into element delcared shadow-root
-    c. use shadow-root name declaration to classify shadow-elements
-        (e.g. shadow-root = "foo" and its shadow-element = "bar", then reference its shadow-element via ::-s-foo-bar)
-  2. web-component: allow author to declare a new element type
-    a. only allowed on single class/attribute selector
-    b. acts as shadow-root
-      (e.g. web-component: Foo, referred to via Foo, resolves to selector of declaration)
-  3. pseudo-states
-    a. same resolution scenario as shadow-elements, but ONLY on shadow-root/web-component
-    b. used via :<pseudo-state> (e.g. :hover)
-*/
-
 module.exports = postcss.plugin('postcss-shadowCSS', function (options) {
+  "use strict";
+
   const opts = options || {}
   const shadowSelectors = [];
+  const SHADOW_SELECTOR_RE = /::-([a-zA-Z]+)-[a-zA-Z\-]+/;
 
-  const registerShadowSelector = (name, alias, warn) => {
-    if (!name || !/::-[a-zA-Z\-]+|^[A-Z][a-zA-Z\-]*|(?: )[A-Z][a-zA-Z\-]*/.test(name)) {
+  const registerShadowSelector = (selector, warn) => {
+    if (!selector.name || !isValidShadowSelector(selector.name)) {
       warn(`
-        Cannot register the shadow selector ${name}.
-        Shadow selectors must be either component selectors ([A-Z][a-zA-Z\-]*)
-        or pseudo-selectors (::-[a-zA-Z\-]+). No underscores or digits are allowed.
+        Cannot register the shadow selector "${selector.name}".
+        Shadow selectors must be pseudo-selectors (::-[a-zA-Z\\-]+).
+        Shadow selectors must not start with a known vendor prefix (like "-webkit-").
+        No underscores or digits are allowed.
       `);
-    } else if (!alias) {
-      warn(`Cannot register the shadow selector ${name} without an alias.`)
-    } else if (shadowSelectors.indexOf(name) > -1) {
-      warn(`
-        Shadow selector ${name} already registered as aliasing ${shadowSelectors[name]}.
-        Cannot register as alias for ${alias}
-      `);
+    } else if (!selector.alias) {
+      warn(`Cannot register the shadow selector "${selector.name}" without an alias.`)
     } else {
-      shadowSelectors.push({name, alias});
+      shadowSelectors.push(selector);
     }
   };
 
   const resolveShadowSelectors = (selector, warn) => {
-    const shadowSelectorRE = /::-[a-zA-Z\-]+|^[A-Z][a-zA-Z\-]*|(?: )[A-Z][a-zA-Z\-]*/;
-    if (shadowSelectorRE.test(selector)) {
+    const shadowSelector = getShadowSelector(selector);
+    if (isValidShadowSelector(shadowSelector)) {
       try {
-        const shadowSelector = shadowSelectorRE.exec(selector)[0].trim();
         const alias = shadowSelectors.find(c => c.name === shadowSelector).alias;
         return resolveShadowSelectors(selector.replace(shadowSelector, alias), warn);
       } catch (e) {
-        warn(`Could not resolve selector: ${selector}\n${e}`);
+        warn(`Could not resolve selector "${selector}" because ${e}.`);
       }
     }
     return selector;
   };
 
-  const resetInheritablePropsForRule = (rule) => {
-    const inheritableProps = [
-      'azimuth', 'border-collapse', 'border-spacing', 'caption-side', 'color',
-      'cursor', 'direction', 'elevation', 'empty-cells', 'font-family',
-      'font-size', 'font-style', 'font-variant', 'font-weight', 'font',
-      'letter-spacing', 'line-height', 'list-style-image',
-      'list-style-position', 'list-style-type', 'list-style', 'orphans',
-      'pitch-range', 'pitch', 'quotes', 'richness', 'speak-header',
-      'speak-numeral', 'speak-punctuation', 'speak', 'speech-rate', 'stress',
-      'text-align', 'text-indent', 'text-transform', 'visibility',
-      'voice-family', 'volume', 'white-space', 'word-spacing'
-    ];
+  const getShadowSelector = (selector) => (
+    SHADOW_SELECTOR_RE.test(selector) ? SHADOW_SELECTOR_RE.exec(selector)[0] : ''
+  );
 
-    inheritableProps.forEach((p) => rule.prepend({prop: p, value: 'initial'}));
-  };
+  const getShadowRoot = (selector) => (
+    SHADOW_SELECTOR_RE.test(selector) ? SHADOW_SELECTOR_RE.exec(selector)[1] : ''
+  );
 
-  const isKnownVendorPrefix = (shadowSelector, warn) => {
-    const knownVendorPrefixes = ['ms', 'mso', 'moz', 'o', 'xv', 'atsc', 'wap', 'webkit', 'khtml', 'konq', 'apple', 'prince', 'ah', 'hp', 'ro', 'rim', 'tc'];
-    if (knownVendorPrefixes.indexOf(shadowSelector) > -1) {
-      warn(`Cannot declare "${shadowSelector}" as a shadow-root. It is a known vendor prefix, already in use.`);
-      return true;
-    }
-    return false;
-  };
-
-  const isFlatSelector = (selector, shadowSelector, warn) => {
-    if (/^[#\.A-Z][\-_a-zA-Z0-9]*$|^\[[^\]]+\]$/.test(rule.selector)) {
-      return true;
-    }
-    warn(`Cannot declare "${shadowSelector}" to be a shadow-root when its alias ("${selector}") is not a single-depth id, class, or attribute selector.`);
-    return false;
-  };
+  const isValidShadowSelector = (selector) => ([
+    'ms', 'mso', 'moz', 'o', 'xv', 'atsc', 'wap', 'webkit', 'khtml', 'konq',
+    'apple', 'prince', 'ah', 'hp', 'ro', 'rim', 'tc'
+  ].indexOf(getShadowRoot(selector)) === -1);
 
   return (root, result) => {
     let shadowPrefix = `::-${opts.shadowPrefix || 's'}-`;
+
     // Find all shadow declarations
     root.walkRules(rule => {
       rule.walkDecls(decl => {
-        const warn = (err) => rule.warn(result, err);
-        if (decl.prop === 'shadow-root') {
-          if (!isKnownVendorPrefix(decl.value, warn) && isFlatSelector(rule.selector, decl.value, warn)) {
-            // Set new shadow root
-            shadowPrefix = `::-${decl.value}-`;
-            // Push Web Component selector name and alias
-            registerShadowSelector(
-              `${decl.value[0].toUpperCase()}${decl.value.slice(1)}`,
-              rule.selector,
-              warn
-            );
-            // Create shadow boundary, if desired AND indicated
-            if (opts.shadowBoundary) {
-              const next = decl.next();
-              if (next && next.prop === 'all' && next.value === 'initial') {
-                resetInheritablePropsForRule(rule);
-                next.remove();
-              }
-            }
-          }
-          decl.remove();
-        }
         if (decl.prop === 'shadow-element') {
-          registerShadowSelector(shadowPrefix + decl.value, rule.selector, warn);
+          registerShadowSelector({name: shadowPrefix + decl.value, alias: rule.selector}, (err) => rule.warn(result, err));
           decl.remove();
         }
       });
@@ -123,8 +63,7 @@ module.exports = postcss.plugin('postcss-shadowCSS', function (options) {
     
     // Replace shadow selectors with their aliases
     root.walkRules(rule => {
-      rule.selector = resolveShadowSelectors(rule.selector, err => rule.warn(result, err));
+      rule.selector = resolveShadowSelectors(rule.selector, (err) => rule.warn(result, err));
     });
   };
 });
-
--- test.js
+++ test.js
@@ -3,32 +3,134 @@ import test    from 'ava';
 
 import plugin from './';
 
-function run(t, input, output, opts) {
-  return postcss([ plugin(opts || {}) ]).process(input)
-    .then( result => {
-      t.deepEqual(result.css, output);
-      t.deepEqual(result.warnings().length, 0);
-    });
+function process(input, opts) {
+  return postcss([ plugin(opts || {}) ]).process(input);
 }
 
 /* Write tests here*/
 
-test('translates a shadow selector into its declared alias', t => {
+test('translates a shadow-element selector into its declared alias', (assert) => {
   const input = `
-  .foo {
-    shadow-element: foo;
-  }
+    .foo {
+      shadow-element: foo;
+    }
 
-  .bar::-s-foo {
-    font-size: 2em;
-  }`;
+    .bar::-s-foo {
+      font-size: 2em;
+    }`;
   const expectedOutput = `
-  .foo {
-  }
+    .foo {
+    }
 
-  .bar.foo {
-    font-size: 2em;
-  }`;
+    .bar.foo {
+      font-size: 2em;
+    }`;
 
-  return run(t, input, expectedOutput, { });
+  return process(input)
+    .then(result => {
+      assert.deepEqual(result.css, expectedOutput);
+      assert.deepEqual(result.warnings().length, 0);
+    });
 });
+
+test('warns for unknown shadow-element selector', (assert) => {
+  const input = `
+    .bar::-s-foo {
+      font-size: 2em;
+    }`;
+
+  return process(input)
+    .then(result => {
+      assert.deepEqual(result.warnings().length, 1);
+    });
+});
+
+test('warns for unknown nested shadow-element selector', (assert) => {
+  const input = `
+    .bar::-s-foo {
+      font-size: 2em;
+    }
+
+    .foo::-s-baz {
+      shadow-element: foo;
+    }`;
+
+  return process(input)
+    .then(result => {
+      assert.deepEqual(result.warnings().length, 2);
+    });
+});
+
+test('ignores known pseudo-element selector', (assert) => {
+  const input = `
+    .bar::-s-foo {
+      display: none;
+    }
+
+    .foo::-webkit-inner-spinner {
+      shadow-element: foo;
+    }`;
+  const expectedOutput = `
+    .bar.foo::-webkit-inner-spinner {
+      display: none;
+    }
+
+    .foo::-webkit-inner-spinner {
+    }`;
+  return process(input)
+    .then(result => {
+      assert.deepEqual(result.css, expectedOutput);
+      assert.deepEqual(result.warnings().length, 0);
+    });
+});
+
+test('warns if known vendor prefix is declared as a shadow-root', (assert) => {
+  const input = `
+    .foo {
+      shadow-root: webkit;
+    }`;
+
+  return process(input)
+    .then(result => {
+      assert.deepEqual(result.warnings().length, 1);
+    });
+});
+
+test('warns if shadow-root alias is not a single-depth id, class, or attribute selector', (assert) => {
+  const input = `
+    .foo .bar {
+      shadow-root: foo;
+    }`;
+
+  return process(input)
+    .then(result => {
+      assert.deepEqual(result.warnings().length, 1);
+    });
+});
+
+test('translates a shadow-root selector into its declared alias', (assert) => {
+  const input = `
+    .foo {
+      shadow-root: foo;
+    }
+
+    Foo.bar {
+      font-size: 2em;
+    }`;
+  const expectedOutput = `
+    .foo {
+    }
+
+    .foo.bar {
+      font-size: 2em;
+    }`;
+
+  return process(input)
+    .then(result => {
+      assert.deepEqual(result.css, expectedOutput);
+      assert.deepEqual(result.warnings().length, 0);
+    });
+});
+
+'\n    .bar::-s-foo {\n      display: none;\n    }\n\n    .foo::-webkit-inner-spinner {\n    }'
+'\n    .bar.foo::-webkit-inner-spinner {\n      display: none;\n    }\n\n    .foo::-webkit-inner-spinner {\n    }'
